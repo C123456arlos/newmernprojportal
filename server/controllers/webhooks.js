4a36c7dcde45d81ee899231a8a0de8ae9b72043b
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-16 19:20:42 -0400

    first commit

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,14 +1,17 @@
 import { Webhook } from "svix"
 import User from "../models/User.js"
 export const clerkWebhooks = async (req, res) => {
+    console.log(req, 'req')
     try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        console.log(whook)
         await whook.verify(JSON.stringify(req.body), {
-            'svix-id': req.headers('svix-id'),
+            'svix-id': req.headers['svix-id'],
             'svix-timestamp': req.headers['svix-timestamp'],
             'svix-signature':req.headers['svix-signature']
         })
         const { data, type } = req.body
+        console.log(data, 'data')
         switch (type) {
             case 'user.created': {
                 const userData = {
